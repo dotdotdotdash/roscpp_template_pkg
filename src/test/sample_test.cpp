64cb341e047c0fedff45a108dd447df2550c750d
Venkatavaradhan V Lakshminarayanan <vvlakshm@asu.edu>
2019-02-21 08:39:38 -0600

    Update sample_test.cpp

--- src/test/sample_test.cpp
+++ src/test/sample_test.cpp
@@ -1,16 +1,3 @@
-// Copyright 2018 Venkisagunner. All rights reserved.
-/*! 
- *  \brief     A brief class description
- *  \details   A detailed explanation
- *  \author    First author name
- *  \author    Second author name(optional)
- *  \version   v1.0.0
- *  \date      MM/DD/YYYY
- *  \pre       Anything that should be presumed or carried out before ?
- *  \bug       Bugs present in the code.
- *  \warning   Warning that should be mentioned.
- *  \copyright © Venkisagunner
- */
 #include <ros/ros.h>
 #include <gtest/gtest.h>
 TEST(TestSuite, template_pkg)
